ffcb2b48684f832a077a5b4d89296e32b2f6c789
Gabriel Gonçalves Machado <gabriel.machado@compasso.com.br>
2023-03-01 10:38:32 -0300

    setup: Initial commit

---
+++ src/test/java/br/com/gabriel/security/api/SecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.security.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
